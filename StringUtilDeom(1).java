fec549abbd2d35f9f6e0291a7c0b7fc14e07ab8e
wei225 <57899485+wei225@users.noreply.github.com>
2020-04-30 23:53:54 +0800

    Update StringUtilDeom(1).java

--- StringUtilDeom(1).java
+++ StringUtilDeom(1).java
@@ -1,10 +1,13 @@
-package com.poplar.bean;
+她说她一直是他的:
+package gushici;
+
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.rmi.MarshalException;
 import java.util.Scanner;
 
-public class StringUtilDeom {
+public class zifuchuan {
 
   //输出文件保存的路径
   public static final String filepath = "D:\\format.txt";
@@ -22,100 +25,70 @@ public class StringUtilDeom {
     String srcStr = scanner.nextLine();
 
     // 第二个参数作为统计的目标字段
-    System.out.println("请输入需要查询的字段，不需要查询请直接回车：");
+    System.out.println("请输入需要查询的字词：");
     String target = scanner.nextLine();
 
     formatAndQuery(srcStr,target);
 
   }
 
-	private static void formatAndQuery(String srcStr, String target) {
-		// 如果输入的诗词原文为空，提示错误信息结束
-		try {
-			if (srcStr == null || srcStr.length() == 0) {
-				throw new MyException("输入诗词原文为空！");
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-
-		// 输出诗词原文
-		System.out.println("====================诗词原文======================");
-		System.out.println(srcStr);
-		System.out.println("=====================诗词原文结束===================");
-
-		// 用于接受结果字符串
-		StringBuilder rsStr = new StringBuilder();
-
-		// 分割原诗词，每特定句长添加一个标点,最多循环原文长度除以每句的长度
-		for (int i = 0; i < (srcStr.length() / SENTENCE_LENGTH); i++) {
-			// 结果字符串拼接从原文截取的字符串
-			rsStr = rsStr.append(srcStr, SENTENCE_LENGTH * i, SENTENCE_LENGTH * (i + 1));
-			// 因为从0开始，所以第0、2、4这类偶数的添加逗号
-			if (i % 2 == 0) {
-				rsStr.append(COMMA);
-			} else { // 1、3、5奇数添加句号和换行符
-				rsStr.append(PERIOD);
-			}
-		}
-
-		// 如果不能整除余下的原文单独成为一行
-		if (srcStr.length() % SENTENCE_LENGTH != 0) {
-			rsStr.append(srcStr.substring(SENTENCE_LENGTH * (srcStr.length() / SENTENCE_LENGTH)))
-					.append(PERIOD);
-		}
-
-		// 输出结果
-		System.out.println("=================格式化后诗词==========================");
-		System.out.print(rsStr);
-		System.out.println("================格式化后诗词结束========================");
-
-		System.out.println("=======================================================");
-
-		System.out.println("================开始写入文件========================");
-		FileWriter fileWriter = null;
-		try {
-			fileWriter = new FileWriter(filepath, false);
-			fileWriter.write(String.valueOf(rsStr));
-		} catch (IOException e) {
-			System.out.println("================写入文件失败========================");
-			e.printStackTrace();
-		} finally {
-			try {
-				fileWriter.flush();
-				fileWriter.close();
-			} catch (IOException e) {
-				System.out.println("================写入文件失败========================");
-				e.printStackTrace();
-			}
-		}
-		System.out.println("================写入文件成功========================");
-		// 如果目标字段为空提示错误信息
-		if (target == null || target.length() == 0) {
-			System.out.println("统计目标字段为空！");
-			return;
-		} else { // 否则展示目标字段
-			System.out.println("统计目标字段为是：" + target);
-		}
-
-		// 统计次数
-		int count = 0;
-		// 坐标
-		int index = 0;
-		while ((index = srcStr.indexOf(target, index)) != -1) {// 如果目标字段target在原文中存在
-			index = index + target.length();
-			count++;// 找到一次统计一次
-		}
-
-		// 输出统计结果
-		System.out.print("目标字段为\"" + target + "\",共出现" + count + "次！");
-	}
-}
-
-class MyException extends RuntimeException {
-
-  public MyException(String message) {
-    super(message);
+ private static void formatAndQuery(String srcStr, String target) {
+  // 如果输入的诗词原文为空，提示错误信息结束
+  try {
+   if (srcStr == null || srcStr.length() == 0) {
+    throw new MarshalException("输入诗词原文为空！");
+   }
+  } catch (Exception e) {
+   System.out.println(e.getMessage());
   }
-}
+  int c3=0;
+  if(target!=null&&!"".equals(target)){
+  for(int y=0;y<srcStr.length();y++){
+	  int c=target.length();
+	  String cwed=srcStr.substring(y);
+	  if(c-1+y<srcStr.length()){
+	//	  String  ydc=srcStr.substring(y, y+c);
+	 if(target.equals(srcStr.substring(y, y+c))){
+		 
+		  c3++;
+	  }
+	  }
+  }
+ 
+  System.out.println(target+"该字段出现的次数为"+""+c3+"次");
+  }
+  // 输出诗词原文
+  
+  
+ 
 
+  // 用于接受结果字符串
+  StringBuilder rsStr = new StringBuilder();
+
+  // 分割原诗词，每特定句长添加一个标点,最多循环原文长度除以每句的长度
+  for (int i = 0; i < (srcStr.length() / SENTENCE_LENGTH); i++) {
+   // 结果字符串拼接从原文截取的字符串
+   rsStr = rsStr.append(srcStr, SENTENCE_LENGTH * i, SENTENCE_LENGTH * (i + 1));
+   // 因为从0开始，所以第0、2、4这类偶数的添加逗号
+   if (i % 2 == 0) {
+    rsStr.append(COMMA);
+   } else { // 1、3、5奇数添加句号和换行符
+    rsStr.append(PERIOD);
+   }
+  }
+
+  // 如果不能整除余下的原文单独成为一行
+  if (srcStr.length() % SENTENCE_LENGTH != 0) {
+   rsStr.append(srcStr.substring(SENTENCE_LENGTH * (srcStr.length() / SENTENCE_LENGTH)))
+     .append(PERIOD);
+  }
+
+  // 输出结果
+  System.out.println("===========
+
+她说她一直是他的:
+====七言律诗格式==========================");
+  System.out.print(rsStr);
+ 
+ }
+}
